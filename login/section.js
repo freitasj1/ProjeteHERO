5af232ae03a2d128b8103e7f17663c0e05b77497
freitasj1 <joaopedromacielfreitas@outlook.com>
2023-09-22 01:02:41 -0300

    teste da função redirecionar

---
+++ login/section.js
@@ -0,0 +1,16 @@
+function redirecionar1() {
+
+    location.href = 'login\section-one.html';
+}
+function redirecionar2() {
+    
+    location.href = 'login\section-two.html';
+}
+function redirecionar3() {
+    
+    location.href = 'section-three.html';
+}
+function redirecionar4() {
+    
+    location.href = 'login\section-four.html';
+}
